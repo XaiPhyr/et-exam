ee7b0091afcb9064a36b58e7baa572ad7d2e0281
Rinise <xrinise@gmail.com>
2020-03-20 00:51:51 +0800

    Commit to master

---
+++ plugins/google/config.php
@@ -0,0 +1,10 @@
+<?php
+	session_start();
+	require_once "GoogleAPI/vendor/autoload.php";
+	$gClient = new Google_Client();
+	$gClient->setClientId("786789161942-bqu33abpcios8i30qp6umf09n89j8j7j.apps.googleusercontent.com");
+	$gClient->setClientSecret("R7j2wpd1-qzGBioX_5nmhGfk");
+	$gClient->setApplicationName("google login");
+	$gClient->setRedirectUri("http://localhost/apiy/");
+	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
+?>
