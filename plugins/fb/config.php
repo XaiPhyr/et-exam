b1177e5561e257233d94ed51c95efa06eab7bbcb
Rinise <xrinise@gmail.com>
2020-03-20 12:58:33 +0800

    Update config.php

--- plugins/fb/config.php
+++ plugins/fb/config.php
@@ -2,9 +2,9 @@
 session_start();
 require_once('Facebook/autoload.php');
 
-$FBObject = new Facebook\Facebook([
-	'app_id' => '2546950908880583',
-	'app_secret' => '858394ed89aab501bf08f592e2e873c3',
+$FBObject = new \Facebook\Facebook([
+	'app_id' => '2624479447832059',
+	'app_secret' => 'c6a246e14976dd665431f2543bcc5f18',
 	'default_graph_version' => 'v2.10'
 ]);
 
